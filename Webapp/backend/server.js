60b0b3944135bf511af3bd323225930c808a8460
ArmanJ04 <dzhansatov00@gmail.com>
2025-06-11 16:03:28 +0500

    ready to defence

--- Webapp/backend/server.js
+++ Webapp/backend/server.js
@@ -18,7 +18,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 const corsOptions = {
-  origin: process.env.CLIENT_URL,
+  origin: "https://cardiohealth-hbwy.onrender.com",
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
