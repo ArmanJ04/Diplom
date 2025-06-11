15bce31c20138d804b8e37ba821777b366cafc0e
ArmanJ04 <dzhansatov00@gmail.com>
2025-06-11 16:17:30 +0500

    ready to defence

--- Webapp/backend/server.js
+++ Webapp/backend/server.js
@@ -18,7 +18,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 const corsOptions = {
-  origin: "https://cardiohealth-hbwy.onrender.com",
+origin: "https://cardiohealth-hbwy.onrender.com"
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
@@ -26,8 +26,16 @@ const corsOptions = {
 
 app.use(cors(corsOptions)); // ✅ must come before routes or parsing
 app.options("*", cors(corsOptions)); // ✅ <-- add this line
+
 app.use(express.json());
 app.use(cookieParser());
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "https://cardiohealth-hbwy.onrender.com");
+  res.header("Access-Control-Allow-Credentials", "true");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
+});
 
 // MongoDB Connection
 mongoose.connect(process.env.MONGO_URI, {
